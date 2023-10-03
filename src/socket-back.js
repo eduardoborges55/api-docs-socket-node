65dc8507680c1e6a3c78131ba5a2001529d8cb6d
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-03 13:50:45 -0300

    feat: add socket listener event by emit textEditor value

--- src/socket-back.js
+++ src/socket-back.js
@@ -3,5 +3,9 @@ import io from '../src/servidor.js'
 
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID:", socket.id);
+
+    socket.on("texto_editor", (texto)=>{
+      console.log(texto)
+    })
   });
 

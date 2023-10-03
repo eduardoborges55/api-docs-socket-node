a6b74e36b046d740082d812b848101032366d2dd
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-03 18:19:50 -0300

    feat: separating all the rooms events by documents names

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -1,11 +1,18 @@
+import { atualizaTextoEditor } from "./documento.js";
+
 const socket = io();
 
-function emitirTextoEditor(texto) {
-    socket.emit("texto_editor", texto)
+function selecionarDocumento(nome) {
+    socket.emit("selecionar_documento", nome);
+}
+
+
+function emitirTextoEditor(texto, nomeDocumento) {
+    socket.emit("texto_editor", texto, nomeDocumento);
 }
 
 socket.on("texto_editor_clientes", (texto) => {
+    atualizaTextoEditor(texto);
+});
 
-})
-
-export { emitirTextoEditor };
+export { emitirTextoEditor, selecionarDocumento };
--- src/socket-back.js
+++ src/socket-back.js
@@ -4,8 +4,13 @@ import io from '../src/servidor.js'
 io.on("connection", (socket) => {
   console.log("Um cliente se conectou! ID:", socket.id);
 
-  socket.on("texto_editor", (texto) => {
-    socket.broadcast.emit("texto_editor_clientes", texto)
+  socket.on("selecionar_documento", (nomeDocumento) => {
+    socket.join(nomeDocumento);
+  })
+
+  socket.on("texto_editor", (texto, nomeDocumento) => {
+    // socket.broadcast.emit("texto_editor_clientes", texto);
+    socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
   })
 });
 

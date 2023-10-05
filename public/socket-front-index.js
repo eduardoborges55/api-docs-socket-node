c7268cbf16751f88cd7e381bf3e41f6bee4398ee
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-05 16:14:01 -0300

    feat: create emit event adding document

--- public/socket-front-index.js
+++ public/socket-front-index.js
@@ -1,5 +1,4 @@
 import { inserirLinkDocumento } from "./index.js";
-
 const socket = io();
 
 socket.emit("obter_documentos", (documentos) => {
@@ -8,3 +7,12 @@ socket.emit("obter_documentos", (documentos) => {
     })
 });
 
+function emitirAdicionarDocumento(nome) {
+    socket.emit("adicionar_documento", nome)
+};
+
+socket.on("adicionar_documento_interface", (nome) => {
+    inserirLinkDocumento(nome)
+});
+
+export { emitirAdicionarDocumento };

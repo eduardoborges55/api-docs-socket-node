dd8714e45bc9212e805950b2042bae7121db6c63
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-05 16:15:36 -0300

    feat: create socket on to emit event and add document

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,10 +1,17 @@
 import io from '../src/servidor.js'
-import { encontrarDocumento, atualizaDocumento, obterDocumentos } from './documentosDb.js';
+import { encontrarDocumento, atualizaDocumento, obterDocumentos, adicionarDocumento } from './documentosDb.js';
 
 io.on("connection", (socket) => {
   socket.on("obter_documentos", async (devolverDocumentos) => {
     const documentos = await obterDocumentos()
     devolverDocumentos(documentos)
+  })
+
+  socket.on("adicionar_documento", async (nome) => {
+    const resultado = await adicionarDocumento(nome)
+    if(resultado.acknowledged){
+      io.emit("adicionar_documento_interface", nome)
+    }
   })
 
   socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {

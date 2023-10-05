840f5598af567e7600055dffc7eb13c96a9cd73f
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-05 15:20:36 -0300

    feat: create take documents server side and remove hard coded documents

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,43 +1,25 @@
 import io from '../src/servidor.js'
-
-const documentos = [
-  {
-    nome: "JavaScript",
-    texto: "texto de javascript..."
-  },
-  {
-    nome: "Node",
-    texto: "texto de node..."
-  },
-  {
-    nome: "Socket.io",
-    texto: "texto de socket.io..."
-  },
-]
+import { encontrarDocumento, atualizaDocumento, obterDocumentos } from './documentosDb.js';
 
 io.on("connection", (socket) => {
-  console.log("Um cliente se conectou! ID:", socket.id);
+  socket.on("obter_documentos", async (devolverDocumentos) => {
+    const documentos = await obterDocumentos()
+    devolverDocumentos(documentos)
+  })
 
-  socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
+  socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {
     socket.join(nomeDocumento);
-    const documento = encontrarDocumento(nomeDocumento);
+    const documento = await encontrarDocumento(nomeDocumento);
     if (documento) {
       devolverTexto(documento.texto);
     }
   })
 
-  socket.on("texto_editor", (texto, nomeDocumento) => {
-    const documento = encontrarDocumento(nomeDocumento);
-    if (documento) {
-      documento.texto = texto;
+  socket.on("texto_editor", async (texto, nomeDocumento) => {
+    const atualizacao = await atualizaDocumento(nomeDocumento, texto);
+    if (atualizacao.modifiedCount) {
       socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
     }
   })
 });
 
-function encontrarDocumento(nome) {
-  const documento = documentos.find((documento) => {
-    return documento.nome === nome;
-  })
-  return documento;
-}

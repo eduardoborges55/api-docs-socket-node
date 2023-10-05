cfe771596d3e12e189b2484725c61cbdab7197b0
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-05 16:13:29 -0300

    feat: take forms and input and create form listener to emit function

--- public/index.js
+++ public/index.js
@@ -1,6 +1,14 @@
-import "./socket-front-index.js"
+import { emitirAdicionarDocumento } from "./socket-front-index.js";
 
-const listaDocumentos = document.getElementById("lista-documentos")
+const listaDocumentos = document.getElementById("lista-documentos");
+const form = document.getElementById("form-adiciona-documento")
+const inputDocumento = document.getElementById("input-documento")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    emitirAdicionarDocumento(inputDocumento.value)
+    inputDocumento.value = ""
+})
 
 function inserirLinkDocumento(nomeDocumento) {
     listaDocumentos.innerHTML += `

8c30f1b4f6a0e08793ec8509010c6e59d7ca323a
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-06 17:45:11 -0300

    feat: add registrareventoscadastro import and put into io.on connection

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,9 +1,11 @@
-import registrarEventosDocumento from "./registrarEventos/registrarEventosDocumento.js";
-import registrarEventosInicio from "./registrarEventos/registrarEventosInicio.js";
+import registrarEventosCadastro from "./registrarEventos/Cadastro.js";
+import registrarEventosDocumento from "./registrarEventos/Documento.js";
+import registrarEventosInicio from "./registrarEventos/EventosInicio.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket, io) => {
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
+  registrarEventosCadastro(socket, io);
 });
 

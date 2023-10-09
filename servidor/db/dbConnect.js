86706afff6d46545ff9fb17cdb20e9b496f3862e
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-09 10:19:45 -0300

    feat: create usuarios connection with db

--- servidor/db/dbConnect.js
+++ servidor/db/dbConnect.js
@@ -4,14 +4,16 @@ import "dotenv/config"
 const cliente = new MongoClient(process.env.DB_CONNECTION_STRING)
 
 let documentosColecao;
+let usuariosColecao;
 
 try {
     await cliente.connect();
-    const db = cliente.db("docs-websockets")
-    documentosColecao = db.collection("documentos")
-    console.log("Conectado ao banco de dados com sucesso")
+    const db = cliente.db("docs-websockets");
+    documentosColecao = db.collection("documentos");
+    usuariosColecao = db.collection("usuarios");
+    console.log("Conectado ao banco de dados com sucesso");
 } catch (erro) {
-    console.log(erro)
+    console.log(erro);
 }
 
-export { documentosColecao };
+export { documentosColecao, usuariosColecao };

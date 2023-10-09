ff1ebf791a17e02c9c5736927a12aaeb7b874d3d
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-09 18:09:22 -0300

    feat: add register event login

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,11 +1,13 @@
 import registrarEventosCadastro from "./registrarEventos/Cadastro.js";
 import registrarEventosDocumento from "./registrarEventos/Documento.js";
 import registrarEventosInicio from "./registrarEventos/EventosInicio.js";
+import registrarEventosLogin from "./registrarEventos/Login.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket, io) => {
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
   registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket, io);
 });
 

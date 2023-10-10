3582eb251e86c7006941e37cbdffc98108896971
eduardoborges55 <eduardoemb1997@gmail.com>
2023-10-09 21:11:46 -0300

    feat: add cadatro_sucesso in front_end with alert

--- public/cadastro/socket-front-cadastro.js
+++ public/cadastro/socket-front-cadastro.js
@@ -4,7 +4,10 @@ function emitirCadastrarUsuario(dados) {
     socket.emit("cadastrar_usuario", dados);
 };
 
-socket.on("cadastro_sucesso", () => alert("Cadastro realizado com sucesso!"));
+socket.on("cadastro_sucesso", () => {
+    alert("Cadastro realizado com sucesso!")
+    window.location.href = "/login/index.html"
+});
 socket.on("cadastro_erro", () => alert("Erro no cadastro"));
 socket.on("usuario_ja_existente", () => alert("Usuário já existe!"))
 
